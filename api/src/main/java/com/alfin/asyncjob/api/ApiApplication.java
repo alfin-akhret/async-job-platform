c0dee4c54a4018c22572a8ba62b736e30cc413de
Alfin Akhret <alfin.akhret@gmail.com>
2026-07-15 16:10:16 +0700

    add docs: 002 http request lifecycle

--- api/src/main/java/com/alfin/asyncjob/api/ApiApplication.java
+++ api/src/main/java/com/alfin/asyncjob/api/ApiApplication.java
@@ -2,19 +2,22 @@ package com.alfin.asyncjob.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.alfin.asyncjob.api.service.NotificationService;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(ApiApplication.class, args);
+        /**
+         * ConfigurableApplicationContext context =
+         * SpringApplication.run(ApiApplication.class, args);
+         * 
+         * NotificationService notificationService =
+         * context.getBean(NotificationService.class);
+         * 
+         * System.out.println(notificationService);
+         */
 
-        NotificationService notificationService = context.getBean(NotificationService.class);
-
-        System.out.println(notificationService);
+        SpringApplication.run(ApiApplication.class, args);
 
     }
 }
