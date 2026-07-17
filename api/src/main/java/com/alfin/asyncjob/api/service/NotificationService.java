66d5a8f1855be2c471db8690ad742e0cf5270d6e
Alfin Akhret <alfin.akhret@gmail.com>
2026-07-17 15:33:34 +0700

    add logger to nitificationService

--- api/src/main/java/com/alfin/asyncjob/api/service/NotificationService.java
+++ api/src/main/java/com/alfin/asyncjob/api/service/NotificationService.java
@@ -2,30 +2,33 @@ package com.alfin.asyncjob.api.service;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.alfin.asyncjob.api.dto.request.CreateJobRequest;
 import com.alfin.asyncjob.api.dto.response.CreateJobResponse;
-import com.alfin.asyncjob.api.exception.DuplicateJobException;
+import com.alfin.asyncjob.api.model.JobStatus;
 
 @Service
 public class NotificationService {
 
+    private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
+
     public CreateJobResponse createJob(CreateJobRequest request) {
         String jobId = UUID.randomUUID().toString();
 
-        System.out.println("=== NEW JOB ===");
-        System.out.println(jobId);
-        System.out.println(request);
+        log.info("Creating notification job");
+        log.info("Recipient: {}", request.recipient());
+        log.info("Subject: {}", request.subject());
 
         // throw new JobNotFoundException("123");
-        throw new DuplicateJobException("123");
+        // throw new DuplicateJobException("123");
 
-        /**
-         * return new CreateJobResponse(
-         * jobId,
-         * JobStatus.PENDING);
-         */
+        log.info("Notification job created successfully");
+        return new CreateJobResponse(
+                jobId,
+                JobStatus.PENDING);
     }
 
 }
