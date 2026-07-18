aa82de92ddb9a325583e00394a4ca91825ef941b
Alfin Akhret <alfin.akhret@gmail.com>
2026-07-18 16:57:22 +0700

    add postgresql as db and use flyway as migration tool

---
+++ api/src/main/resources/db/migration/V1__create_jobs_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE jobs (
+    id UUID PRIMARY KEY,
+    recipient VARCHAR(255) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
